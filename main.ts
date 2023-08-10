543de9bbd33ba74dcc75a2275c03c3020b355b0b
picur1 <133989406+picur1@users.noreply.github.com>
2023-08-10 16:09:48 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+function ShowTextList (list: any[]) {
+    for (let value of list) {
+        basic.clearScreen()
+        basic.pause(500)
+    }
+}
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(textlist1.length)
+    ShowTextList(textlist1)
+    basic.pause(500)
+    textlist1.shift()
+    ShowTextList(textlist1)
+    basic.pause(500)
+    textlist1.pop()
+    ShowTextList(textlist1)
+    basic.pause(500)
+    textlist1.insertAt(0, char)
+    ShowTextList(textlist1)
+})
+let char = ""
+let textlist1: string[] = []
+textlist1 = [
+"a",
+"b",
+"c",
+"d"
+]
+char = "x"
+basic.forever(function () {
+	
+})
